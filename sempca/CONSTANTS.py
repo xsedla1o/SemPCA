7d589fa367a6009aacf530cd59331d9e4a8ec16e
Ondrej Sedlacek <xsedla1o@stud.fit.vutbr.cz>
2025-02-25 19:18:24 +0100

    refactor: move to python package structure
    Fix imports without path-hacking.
    Create a `pyproject.toml` for editable installation.
    `PROJECT_ROOT` maintains the same location.

---
+++ sempca/CONSTANTS.py
@@ -0,0 +1,48 @@
+import hashlib
+import os
+import random
+import time
+
+import numpy as np
+import torch
+
+print("Seeding everything...")
+seed = 6
+random.seed(seed)  # Python random module.
+np.random.seed(seed)  # Numpy module.
+torch.manual_seed(seed)  # Torch CPU random seed module.
+torch.cuda.manual_seed(seed)  # Torch GPU random seed module.
+torch.cuda.manual_seed_all(seed)  # Torch multi-GPU random seed module.
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["PYTHONHASHSEED"] = str(seed)
+
+print("Seeding Finished")
+
+# Device configuration
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+SESSION = hashlib.md5(
+    time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time() + 8 * 60 * 60)).encode(
+        "utf-8"
+    )
+).hexdigest()
+SESSION = "SESSION_" + SESSION
+
+
+def GET_PROJECT_ROOT():
+    # The constants file is located in the root of the project
+    package_root = os.path.dirname(os.path.abspath(__file__))
+    return os.path.dirname(package_root)
+
+
+def GET_LOGS_ROOT():
+    log_file_root = os.path.join(GET_PROJECT_ROOT(), "logs")
+    if not os.path.exists(log_file_root):
+        os.makedirs(log_file_root)
+    return log_file_root
+
+
+LOG_ROOT = GET_LOGS_ROOT()
+PROJECT_ROOT = GET_PROJECT_ROOT()
---
+++ sempca/entities/__init__.py
@@ -0,0 +1,7 @@
+from .TensorInstances import (
+    TensorInstance,
+    TInstWithLogits,
+    SequentialTensorInstance,
+    DualTensorInstance,
+)
+from .instances import Instance, SubSequenceInstance, Log_With_Datetime, Log_Time_Step
