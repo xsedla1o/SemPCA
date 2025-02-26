fc106c325c8ca000afae1159781cf9f4e3e7945f
Ondrej Sedlacek <xsedla1o@stud.fit.vutbr.cz>
2025-02-26 14:05:11 +0100

    refactor(models): deduplicate pca code

--- sempca/models/pca.py
+++ sempca/models/pca.py
@@ -16,109 +16,7 @@ import numpy as np
 from sempca.utils import metrics, get_logger
 
 
-class PCAPlusPlus(object):
-    def __init__(self, n_components=0.95, threshold=None, c_alpha=3.2905):
-        self.logger = get_logger("PCAPlusPlus")
-
-        self.proj_C = None
-        self.components = None
-        self.n_components = n_components
-        self.threshold = threshold
-        self.c_alpha = c_alpha
-        self.fixed_threshold = threshold
-
-    def fit(self, X):
-        self.logger.info("====== Model summary ======")
-        num_instances, num_events = X.shape
-        X_cov = np.dot(X.T, X) / float(num_instances)
-        U, sigma, V = np.linalg.svd(X_cov)
-        n_components = self.n_components
-        if n_components < 1:
-            total_variance = np.sum(sigma)
-            variance = 0
-            for i in range(num_events):
-                variance += sigma[i]
-                if variance / total_variance >= n_components:
-                    break
-            n_components = i + 1
-
-        P = U[:, :n_components]
-        I = np.identity(num_events, int)
-        self.components = P
-        self.proj_C = I - np.dot(P, P.T)
-        self.logger.info("n_components: {}".format(n_components))
-        self.logger.info(
-            "Project matrix shape: {}-by-{}".format(
-                self.proj_C.shape[0], self.proj_C.shape[1]
-            )
-        )
-
-        if not self.threshold:
-            phi = np.zeros(3)
-            for i in range(3):
-                for j in range(n_components, num_events):
-                    phi[i] += np.power(sigma[j], i + 1)
-            h0 = 1.0 - 2 * phi[0] * phi[2] / (3.0 * phi[1] * phi[1])
-            self.threshold = phi[0] * np.power(
-                self.c_alpha * np.sqrt(2 * phi[1] * h0 * h0) / phi[0]
-                + 1.0
-                + phi[1] * h0 * (h0 - 1) / (phi[0] * phi[0]),
-                1.0 / h0,
-            )
-        self.logger.info("SPE threshold: {}\n".format(self.threshold))
-
-    def predict(self, X, fixed_threshold=None):
-        assert self.proj_C is not None, (
-            "PCA model needs to be trained before prediction."
-        )
-        y_pred = np.zeros(X.shape[0])
-        for i in range(X.shape[0]):
-            y_a = np.dot(self.proj_C, X[i, :])
-            SPE = np.dot(y_a, y_a)
-            if fixed_threshold is not None and fixed_threshold != -1.0:
-                self.fixed_threshold = fixed_threshold
-            else:
-                self.fixed_threshold = self.threshold
-            if SPE > self.fixed_threshold:
-                y_pred[i] = 1
-        return y_pred
-
-    def evaluate(self, X, y_true, fixed_threshold=None):
-        if fixed_threshold is not None:
-            self.logger.info("Threshold: %.8f" % fixed_threshold)
-        else:
-            self.logger.info(
-                "No given threshold, will be using default PCA threshold: %.8f."
-                % self.threshold
-            )
-            fixed_threshold = self.threshold
-        y_pred = self.predict(X, fixed_threshold)
-        TP, TN, FP, FN = 0, 0, 0, 0
-        for pred, true in zip(y_pred, y_true):
-            if pred == true:
-                if pred == 1:
-                    TP += 1
-                else:
-                    TN += 1
-            else:
-                if pred == 1:
-                    FP += 1
-                else:
-                    FN += 1
-        if TP == 0:
-            precision, recall, f1 = 0, 0, 0
-        else:
-            precision, recall, f1 = metrics(y_pred, y_true)
-        self.logger.info(
-            "Precision: {:.0f}/{:.0f} = {:.4f}, recall: {:.0f}/{:.0f} = {:.4f}, F1-measure: {:.4f}".format(
-                TP, (TP + FP), precision, TP, (TP + FN), recall, f1
-            )
-        )
-
-        return precision, recall, f1
-
-
-class PCA(object):
+class PCA:
     def __init__(self, n_components=0.95, threshold=None, c_alpha=3.2905):
         """The PCA model for anomaly detection
 
@@ -151,7 +49,7 @@ class PCA(object):
 
     def fit(self, X):
         """
-        Auguments
+        Arguments
         ---------
             X: ndarray, the event count matrix of shape num_instances-by-num_events
         """
@@ -246,3 +144,9 @@ class PCA(object):
         )
 
         return precision, recall, f1
+
+
+class PCAPlusPlus(PCA):
+    def __init__(self, n_components=0.95, threshold=None, c_alpha=3.2905):
+        super(PCAPlusPlus, self).__init__(n_components, threshold, c_alpha)
+        self.logger = get_logger("PCAPlusPlus")
